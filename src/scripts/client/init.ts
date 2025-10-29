5741772e61356cd8f2e510f51f5dbceacfef3bc0
☘ <kogawananari@gmail.com>
2025-10-29 15:53:35 +0800

    :heavy_plus_sign:

--- src/scripts/client/init.ts
+++ src/scripts/client/init.ts
@@ -1,3 +1,14 @@
+// CSS Houdini
+import smoothCorners from "smooth-corners?url"
+declare global {
+    namespace CSS {
+        const paintWorklet: Worklet
+    }
+}
+if (CSS && "paintWorklet" in CSS) {
+    CSS.paintWorklet.addModule(smoothCorners)
+}
+
 // 需要引入一下 window上有宣告theme
 import type { Theme as _Theme } from "@holy-two/data-theme"
 import message from "./message"
